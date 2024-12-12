5a9f74d74e33f271979fce69bae486fc4609f7e0
agirongInfa <agirong@bancatlan.hn>
2024-12-11 23:43:11 -0600

    Hacer validaciones en las entities para responder en el response.

--- AppAngular.Server/Entities/Parametro.cs
+++ AppAngular.Server/Entities/Parametro.cs
@@ -8,10 +8,22 @@ namespace AppAngular.Server.Entities
     {
         [Key]
         public long IdParametro { get; set; }
-        public string? Codigo { get; set; }
+        [Required(ErrorMessage = "El codigo es requerido")]
+        [StringLength(3, ErrorMessage = "El codigo debe tener maximo tres caracteres")]
+        public string Codigo { get; set; }
+
+        [Required(ErrorMessage = "El Valor es requerido")]
+        [StringLength(400, ErrorMessage = "El codigo debe tener maximo 400 caracteres")]
         public string Valor { get; set; }
+
+        [Required(ErrorMessage = "La Descricrpcion es requerido")]
+        [StringLength(400, ErrorMessage = "La descripcion debe tener maximo 400 caracteres")]
         public string? Descripcion { get; set; }
+
+        [Required(ErrorMessage = "El estado es requerido")]
         public int Estado { get; set; }
+
+        [StringLength(10, ErrorMessage = "El codigo debe tener maximo diez caracteres")]
         public string? TipoParametro { get; set; }
     }
 }
