69dc9653bc94904067da6c61ac610ac9d0d254e1
agirongInfa <agirong@bancatlan.hn>
2024-12-12 00:55:08 -0600

    Cambio: Capa de Servicio Implementacion que Consulta la capa de Repositorio. IServicioCatalogo que expone los metodos para que lo consulte el controlador.

---
+++ AppAngular.Server/Servicio/IServicioCatalogo.cs
@@ -0,0 +1,13 @@
+﻿using AppAngular.Server.Entities;
+using AppAngular.Server.Utils;
+
+namespace AppAngular.Server.Servicio
+{
+    public interface IServicioCatalogo
+    {
+        public GeneralResponse listaCatalogos();
+        public GeneralResponse guardarCatalogo(Catalogo catalogo);
+        public GeneralResponse actualizarCatalogo(string idCatalogo, Catalogo catalogo);
+        public GeneralResponse eliminarCatalogo(string idCatalogo);
+    }
+}
---
+++ AppAngular.Server/ServicioImp/ServicioCatalogoImp.cs
@@ -0,0 +1,90 @@
+﻿using AppAngular.Server.Entities;
+using AppAngular.Server.Repositorio;
+using AppAngular.Server.Servicio;
+using AppAngular.Server.Utils;
+
+namespace AppAngular.Server.ServicioImp
+{
+    public class ServicioCatalogoImp : IServicioCatalogo
+    {
+        //inyectar el repositorio de catalogo
+        private readonly IRepositorioCatalogo _repositorioCatalogo;
+
+        GeneralResponse generalResponse = new GeneralResponse();    
+        public ServicioCatalogoImp(IRepositorioCatalogo repositorioCatalogo)
+        {
+            _repositorioCatalogo = repositorioCatalogo;
+        }
+        // Aquí vamos a crear nuevos metodos a que consumen de la interfaz de repositorio
+        // La finalidad de personalizar y controlar excepciones. Ya que el usuario no debe conocer que tipo de error sucedió en el backend.
+        public GeneralResponse listaCatalogos()
+        {
+            try
+            {
+                List<Catalogo> list = _repositorioCatalogo.GetCatalogos();
+                if (list.Count() > 0)
+                {
+                    generalResponse = GeneralResponseFn.responseGeneral(Constantes.CODIGO_EXITO, Constantes.MENSAJE_OK, list);
+                }
+                else
+                {
+                    generalResponse = GeneralResponseFn.responseGeneral(Constantes.CODIGO_NO_DATA, Constantes.MENSAJE_NO_DATA, list);
+                }
+            }
+            catch (Exception)
+            {
+                generalResponse = GeneralResponseFn.responseGeneral(Constantes.CODIGO_ERROR, Constantes.getMensaje(""), null);
+            }
+            return generalResponse;
+        }
+
+
+        public GeneralResponse guardarCatalogo(Catalogo catalogo)
+        {
+            try
+            {
+                _repositorioCatalogo.SaveCatalogo(catalogo);
+                generalResponse = GeneralResponseFn.responseGeneral(Constantes.CODIGO_EXITO, Constantes.MENSAJE_OK, null);
+
+            }
+            catch (Exception ex)
+            {
+                generalResponse = GeneralResponseFn.responseGeneral(Constantes.CODIGO_ERROR, Constantes.getMensaje(ex.Message), null);
+            }
+
+            return generalResponse;
+        }
+
+        public GeneralResponse actualizarCatalogo(string idCatalogo,Catalogo catalogo)
+        {
+            try
+            {
+                _repositorioCatalogo.UpdateCatalogo(idCatalogo,catalogo);
+                generalResponse = GeneralResponseFn.responseGeneral(Constantes.CODIGO_EXITO, Constantes.MENSAJE_OK, null);
+
+            }
+            catch (Exception ex)
+            {
+                generalResponse = GeneralResponseFn.responseGeneral(Constantes.CODIGO_ERROR, Constantes.getMensaje(ex.Message), null);
+            }
+
+            return generalResponse;
+        }
+
+        public GeneralResponse eliminarCatalogo(string idCatalogo)
+        {
+            try
+            {
+                _repositorioCatalogo.DeleteCatalogo(idCatalogo);
+                generalResponse = GeneralResponseFn.responseGeneral(Constantes.CODIGO_EXITO, Constantes.MENSAJE_OK, null);
+
+            }
+            catch (Exception ex)
+            {
+                generalResponse = GeneralResponseFn.responseGeneral(Constantes.CODIGO_ERROR, Constantes.getMensaje(ex.Message), null);
+            }
+
+            return generalResponse;
+        }
+    }
+}
