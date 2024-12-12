eca81531db01c0e2116f7393125d693d4a189050
agirongInfa <agirong@bancatlan.hn>
2024-12-12 00:37:59 -0600

    Cambio: Capa de repositorio que contiene las consultas a la base de datos para el CRUD

---
+++ AppAngular.Server/Repositorio/IRepositorioCatalogo.cs
@@ -0,0 +1,13 @@
+﻿using AppAngular.Server.Entities;
+
+namespace AppAngular.Server.Repositorio
+{
+    //Interfaz para exponer los metodos del repositorio a las demas capas de la aplicación. 
+    public interface IRepositorioCatalogo
+    {
+        List<Catalogo> GetCatalogos();
+        void SaveCatalogo(Catalogo catalogo);
+        void UpdateCatalogo(string idCatalogo, Catalogo catalogo); 
+        void DeleteCatalogo(string idCatalogo);
+    }
+}
