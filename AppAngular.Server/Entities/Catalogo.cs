aaa1c65040a617fc968bd0674aac8e560ca644c0
agirongInfa <agirong@bancatlan.hn>
2024-12-12 00:03:45 -0600

    Cambio: Agregar Entidad de Tabla

---
+++ AppAngular.Server/Entities/Catalogo.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AppAngular.Server.Entities
+{
+    [Table("Catalogo")]
+    public class Catalogo
+    {
+        [Key]
+        public string IdCatalogo { get; set; }
+        public string? Descripcion { get; set; }
+        public int? Estado { get; set; }
+    }
+}
